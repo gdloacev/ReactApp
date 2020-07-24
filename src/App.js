39db8aa73be721796066ca16fd183a29c23a1521
gdloacev <itan040@hotmail.com>
2020-07-24 14:36:23 -0500

    Change Calc

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Header from './components/Header';
 function App() {
 
   const name = 'Oscar Aceves';
-  const calc = 8*3;
+  const calc = 8*4;
 
   return (
     <div className="App-header">
